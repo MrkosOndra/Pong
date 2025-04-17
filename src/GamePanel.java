0a563775e129a694be496495c17f292db0e07f00
MrkosOndra <mrkos@spsejecna.cz>
2025-04-17 16:49:40 +0200

    vytvoreni tridu Paddle.java kde vytvarime palku podle veliksoti obrazovky a metody k pohybu

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,17 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
-    public GamePanel(){
+    private Paddle player;
+
+    private int screenWidth;
+    private int screenHeight;
+
+    public GamePanel() {
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
+        this.setLayout(null);
+
+        // chat gpt
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = screenSize.width;
+        screenHeight = screenSize.height;
+
+
+        int paddleWidth = screenWidth / 60;
+        int paddleHeight = screenHeight / 5;
+        int paddleX = screenWidth / 30;
+        int paddleY = screenHeight / 2 - paddleHeight / 2;
+
+        player = new Paddle(paddleX, paddleY, paddleWidth, paddleHeight);
     }
 //chat gpt
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        g.setColor(Color.WHITE);
-        g.fillRect(100, 100, 20, 80);
-        g.fillOval(200, 200, 20, 20);
+        player.draw(g);
     }
 }
---
+++ src/Paddle.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+
+public class Paddle {
+    private int x, y;
+    private int width;
+    private int height;
+    private int speed = 10;
+
+    public Paddle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public void draw(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, width, height);
+    }
+    public void moveUp() {
+        y -= speed;
+        if (y < 0) y = 0;
+    }
+
+    public void moveDown(int panelHeight) {
+        if (y + height < panelHeight) {
+            y += speed;
+        }
+    }
+
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+}
+
