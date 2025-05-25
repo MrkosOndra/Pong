296c290e991a9482465f344b2a6555d1a326a021
MrkosOndra <mrkos@spsejecna.cz>
2025-05-25 17:17:18 +0200

    pridani Panelu ve kterem jsou tlacitka s ruznymi barvami palek a metody handlePaddleColorSelection ktera je stejna jako handleBallSelection ale pro palku

--- src/ShopPanel.java
+++ src/ShopPanel.java
@@ -48,6 +48,38 @@ public class ShopPanel extends JPanel {
 
         add(Box.createVerticalStrut(30));
 
+        JLabel paddleLabel = new JLabel("Vyber barvu pálky:");
+        paddleLabel.setForeground(Color.WHITE);
+        paddleLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        paddleLabel.setAlignmentX(CENTER_ALIGNMENT);
+        add(paddleLabel);
+
+        JPanel paddlePanel = new JPanel();
+        paddlePanel.setBackground(Color.BLACK);
+
+        JButton redBtn = new JButton();
+        redBtn.setBackground(Color.RED);
+        redBtn.setPreferredSize(new Dimension(50, 50));
+        redBtn.setFocusPainted(false);
+
+        JButton blueBtn = new JButton();
+        blueBtn.setBackground(Color.BLUE);
+        blueBtn.setPreferredSize(new Dimension(50, 50));
+        blueBtn.setFocusPainted(false);
+
+        JButton greenBtn = new JButton();
+        greenBtn.setBackground(Color.GREEN);
+        greenBtn.setPreferredSize(new Dimension(50, 50));
+        greenBtn.setFocusPainted(false);
+
+        paddlePanel.add(redBtn);
+        paddlePanel.add(blueBtn);
+        paddlePanel.add(greenBtn);
+        add(paddlePanel);
+
+        redBtn.addActionListener(e -> handlePaddleColorSelection(Color.RED, coinsLabel));
+        blueBtn.addActionListener(e -> handlePaddleColorSelection(Color.BLUE, coinsLabel));
+        greenBtn.addActionListener(e -> handlePaddleColorSelection(Color.GREEN, coinsLabel));
     }
     //chat gpt
     private void handleBallSelection(String skinName, JLabel coinsLabel) {
@@ -55,9 +87,24 @@ public class ShopPanel extends JPanel {
             SkinManager.selectBallSkin(skinName);
             JOptionPane.showMessageDialog(this, "Používáš skin: " + skinName);
         } else {
-            boolean unlocked = SkinManager.purchaseBallSkin(skinName);
-            if (unlocked) {
+            boolean unlockedB = SkinManager.purchaseBallSkin(skinName);
+            if (unlockedB) {
                 JOptionPane.showMessageDialog(this, "Koupil jsi skin: " + skinName);
+            } else {
+                JOptionPane.showMessageDialog(this, "Nedostatek bodů!");
+            }
+        }
+
+        coinsLabel.setText("Máš " + SkinManager.getCoins() + " bodů");
+    }
+    private void handlePaddleColorSelection(Color color, JLabel coinsLabel) {
+        if (SkinManager.isPaddleColorUnlocked(color)) {
+            SkinManager.purchasePaddleColor(color);
+            JOptionPane.showMessageDialog(this, "Používáš novou barvu pálky!");
+        } else {
+            boolean unlockedP = SkinManager.purchasePaddleColor(color);
+            if (unlockedP) {
+                JOptionPane.showMessageDialog(this, "Zakoupil jsi novou barvu pálky!");
             } else {
                 JOptionPane.showMessageDialog(this, "Nedostatek bodů!");
             }
