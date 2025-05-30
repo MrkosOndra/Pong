a10c93dff2f57d87d4323d3459db22d59f0e2399
MrkosOndra <mrkos@spsejecna.cz>
2025-05-30 20:53:28 +0200

    JavaDocs

--- src/PongGame/Entities/Ball.java
+++ src/PongGame/Entities/Ball.java
@@ -3,7 +3,9 @@ package PongGame.Entities;
 import PongGame.Managers.SkinManager;
 
 import java.awt.*;
-
+/**
+ * Represents the ball used in the Pong game.
+ */
 public class Ball {
     private Color color;
 
@@ -13,6 +15,14 @@ public class Ball {
     private int speed = 5;
     private int maxSpeed=15;
 
+    /**
+     * Constructs a new Ball with specified position and size.
+     *
+     * @param x      the initial x position of the ball
+     * @param y      the initial y position of the ball
+     * @param width  the width of the ball
+     * @param height the height of the ball
+     */
     public Ball(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
@@ -23,20 +33,42 @@ public class Ball {
         xVelocity= speed;
         yVelocity= speed;
     }
+
+    /**
+     * Moves the ball by updating its position based on current velocity.
+     */
 public void move(){
     x += xVelocity;
     y += yVelocity;
 }
+    /**
+     * Draws the ball on the screen.
+     *
+     * @param g the Graphics object to draw with
+     */
 public void draw(Graphics g){
     g.setColor(color);
     g.fillOval(x, y, width, height);
 }
+    /**
+     * Reverses the ball's horizontal direction.
+     */
 public void ReverseXDirection(){
 xVelocity=-xVelocity;
 }
+    /**
+     * Reverses the ball's vertical direction.
+     */
 public void ReverseYDirection(){
         yVelocity=-yVelocity;
 }
+    /**
+     * Resets the ball to the center of the screen and sets its direction.
+     *
+     * @param screenWidth   width of the game screen
+     * @param screenHeight  height of the game screen
+     * @param TowardsPlayer true if the ball should move towards the player, false otherwise
+     */
 public void reset(int screenWidth , int screenHeight , boolean TowardsPlayer){
         x=screenWidth / 2 - width / 2;
         y=screenWidth / 2 - height / 2;
@@ -48,6 +80,9 @@ public void reset(int screenWidth , int screenHeight , boolean TowardsPlayer){
         }
     yVelocity = (Math.random() < 0.5) ? speed : -speed;
 }
+    /**
+     * Increases the ball's speed, up to the defined maximum speed.
+     */
     public void increaseSpeed() {
         if (xVelocity > 0 && xVelocity < maxSpeed) {
             xVelocity = xVelocity + 1;
@@ -61,6 +96,9 @@ public void reset(int screenWidth , int screenHeight , boolean TowardsPlayer){
             yVelocity = yVelocity - 1;
         }
     }
+    /**
+     * Resets the ball's speed to its default starting value.
+     */
 public void resetSpeed(){
         speed=5;
 }
--- src/PongGame/Entities/ScoreBoard.java
+++ src/PongGame/Entities/ScoreBoard.java
@@ -1,18 +1,28 @@
 package PongGame.Entities;
 
 import java.awt.*;
-
+/**
+ * Represents the scoreboard displaying the current score of both players.
+ */
 public class ScoreBoard {
     private int playerScore;
     private int aiScore;
-
+    /**
+     * Constructs a new ScoreBoard with both scores initialized to 0.
+     */
     public ScoreBoard() {
         this.playerScore = 0;
         this.aiScore = 0;
     }
+    /**
+     * Increments the score of Player by 1.
+     */
     public void addPointPlayer(){
         playerScore=playerScore+1;
     }
+    /**
+     * Increments the score of Ai by 1.
+     */
     public void addPointAi(){
         aiScore=aiScore+1;
     }
@@ -24,10 +34,13 @@ public class ScoreBoard {
     public int getAiScore() {
         return aiScore;
     }
-    public void resetScore(){
-        playerScore=0;
-        aiScore=0;
-    }
+
+    /**
+     * Draws the current scores on the screen.
+     *
+     * @param g           the Graphics context used to draw
+     * @param screenWidth the width of the screen, used to center the score
+     */
     public void draw(Graphics g, int screenWidth) {
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 100));
