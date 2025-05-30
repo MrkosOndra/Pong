0e6991bb572b66e58f7a760c1124fa7e950409f1
MrkosOndra <mrkos@spsejecna.cz>
2025-05-30 20:10:18 +0200

    JavaDocs

--- src/PongGame/Managers/SkinManager.java
+++ src/PongGame/Managers/SkinManager.java
@@ -1,7 +1,10 @@
 package PongGame.Managers;
 
 import java.awt.*;
-
+/**
+ * The SkinManager class handles unlocking, purchasing, and selecting
+ * paddle and ball colors. It also manages player coins.
+ */
 public class SkinManager {
     private static int coins = 0;
 
@@ -21,7 +24,11 @@ public class SkinManager {
     private static boolean bluePaddleUnlocked = false;
     private static boolean greenPaddleUnlocked = false;
 
-
+    /**
+     * Tries to purchase or select a ball color.
+     * @param color the desired ball color
+     * @return true if the purchase or selection was successful
+     */
     public static boolean purchaseBallColor(Color color) {
         if (isBallColorUnlocked(color)) {
             ballColor = color;
@@ -36,31 +43,50 @@ public class SkinManager {
         }
         return false;
     }
+    /**
+     * Sets the selected ball color if it is already unlocked.
+     * @param color the desired ball color
+     */
     public static void selectBallColor(Color color) {
         if (isBallColorUnlocked(color)) {
             ballColor = color;
         }
     }
+    /**
+     * Sets the selected paddle color if it is already unlocked.
+     * @param color the desired paddle color
+     */
     public static void selectPaddleColor(Color color) {
         if (isPaddleColorUnlocked(color)) {
             paddleColor = color;
         }
     }
-
+    /**
+     * Checks if the given ball color is unlocked.
+     * @param color the ball color to check
+     * @return true if the color is unlocked
+     */
     public static boolean isBallColorUnlocked(Color color) {
         if (color.equals(Color.RED)) return redBallUnlocked;
         if (color.equals(Color.GREEN)) return greenBallUnlocked;
         if (color.equals(Color.YELLOW)) return yellowBallUnlocked;
         return false;
     }
-
+    /**
+     * Unlocks the given ball color.
+     * @param color the color to unlock
+     */
     private static void unlockBallColor(Color color) {
         if (color.equals(Color.RED)) redBallUnlocked = true;
         if (color.equals(Color.GREEN)) greenBallUnlocked = true;
         if (color.equals(Color.YELLOW)) yellowBallUnlocked = true;
     }
 
-
+    /**
+     * Tries to purchase or select a paddle color.
+     * @param color the desired paddle color
+     * @return true if the purchase or selection was successful
+     */
     public static boolean purchasePaddleColor(Color color) {
         if (color.equals(Color.RED)) {
             if (!redPaddleUnlocked && coins >= colorPrice) {
@@ -97,13 +123,21 @@ public class SkinManager {
 
         return false;
     }
-
+    /**
+     * Checks if the given paddle color is unlocked.
+     * @param color the paddle color to check
+     * @return true if the color is unlocked
+     */
     public static boolean isPaddleColorUnlocked(Color color) {
         if (color.equals(Color.RED)) return redPaddleUnlocked;
         if (color.equals(Color.BLUE)) return bluePaddleUnlocked;
         if (color.equals(Color.GREEN)) return greenPaddleUnlocked;
         return false;
     }
+    /**
+     * Adds coins to the player's balance.
+     * @param amount the number of coins to add
+     */
     public static void addCoins(int amount) {
         coins += amount;
     }
